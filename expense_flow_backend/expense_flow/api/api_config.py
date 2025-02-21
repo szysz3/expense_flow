2f865353ea44c703de5ba5673cd1791d3fea33eb
Michal Szyszka <michalszyszka.m@gmail.com>
2025-02-21 19:17:05 +0100

    Update config

--- expense_flow_backend/expense_flow/api/api_config.py
+++ expense_flow_backend/expense_flow/api/api_config.py
@@ -20,26 +20,16 @@ class APIConfig:
     @classmethod
     def from_env(cls) -> 'APIConfig':
         return cls(
-            # max_retries=int(os.getenv('MAX_RETRIES', '3')),
-            # retry_delay=float(os.getenv('RETRY_DELAY', '1.0')),
-            # db_path=os.getenv('DB_PATH', '.data/serve/receipts.db'),
-            # temp_db_path=os.getenv('TEMP_DB_PATH', '.data/serve/temp_receipts.db'),
-            # azure_endpoint=os.getenv('AZURE_DOCUMENT_ENDPOINT', ''),
-            # azure_key=os.getenv('AZURE_DOCUMENT_KEY', ''),
-            # ollama_host=os.getenv('OLLAMA_HOST', 'http://localhost:11434'),
-            # ollama_model=os.getenv('OLLAMA_MODEL', 'hf.co/speakleash/Bielik-11B-v2.3-Instruct-GGUF:Q6_K_low_temp'),
-            # ollama_fallback_model=os.getenv('OLLAMA_FALLBACK_MODEL', 'hf.co/unsloth/phi-4-GGUF:Q5_K_M'),
-            # chatgpt_key=os.getenv('CHATGPT_KEY')
-            max_retries=3,
-            retry_delay=1.0,
-            db_path='.data/serve/receipts.db',
-            temp_db_path='.data/serve/temp_receipts.db',
+            max_retries=int(os.getenv('MAX_RETRIES', '3')),
+            retry_delay=float(os.getenv('RETRY_DELAY', '1.0')),
+            db_path=os.getenv('DB_PATH', '.data/serve/receipts.db'),
+            temp_db_path=os.getenv('TEMP_DB_PATH', '.data/serve/temp_receipts.db'),
             azure_endpoint=os.getenv('AZURE_DOCUMENT_ENDPOINT', ''),
             azure_key=os.getenv('AZURE_DOCUMENT_KEY', ''),
-            ollama_host='http://localhost:11434',
-            ollama_model='hf.co/speakleash/Bielik-11B-v2.3-Instruct-GGUF:Q6_K_low_temp',
-            ollama_fallback_model='hf.co/unsloth/phi-4-GGUF:Q5_K_M',
-            chatgpt_key=os.getenv('CHATGPT_KEY')            
+            ollama_host=os.getenv('OLLAMA_HOST', 'http://localhost:11434'),
+            ollama_model=os.getenv('OLLAMA_MODEL', 'hf.co/speakleash/Bielik-11B-v2.3-Instruct-GGUF:Q6_K_low_temp'),
+            ollama_fallback_model=os.getenv('OLLAMA_FALLBACK_MODEL', 'hf.co/unsloth/phi-4-GGUF:Q5_K_M'),
+            chatgpt_key=os.getenv('CHATGPT_KEY')
         )
 
 @lru_cache()
