67cb02c093ccde0f25c4426bbefc336fb1593f08
Michal Szyszka <michalszyszka.m@gmail.com>
2025-02-26 20:26:27 +0100

    Fix currency on unprocessed screen

--- expense_flow_mobile/packages/presentation/lib/screen/unprocessed_receipts/widget/unprocessed_receipt_item.dart
+++ expense_flow_mobile/packages/presentation/lib/screen/unprocessed_receipts/widget/unprocessed_receipt_item.dart
@@ -75,8 +75,11 @@ class UnprocessedReceiptItem extends StatelessWidget {
 
   Widget _buildAmount(BuildContext context) {
     final amount = receipt.rawData.total;
+    final locale = Localizations.localeOf(context).toString();
+    final currencySymbol = NumberFormat.currency(locale: locale).currencySymbol;
+
     return Text(
-      '\$$amount',
+      '$currencySymbol $amount',
       style: Theme.of(context).textTheme.titleMedium?.copyWith(
             fontWeight: FontWeight.bold,
           ),
