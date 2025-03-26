1435300477a4814ea34ffd9be9278f2e6c16a1cc
Michal Szyszka <michalszyszka.m@gmail.com>
2025-03-26 14:40:41 +0100

    Remove background camera preview screen

--- expense_flow_mobile/packages/presentation/lib/screen/main/main_screen.dart
+++ expense_flow_mobile/packages/presentation/lib/screen/main/main_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,7 +6,6 @@ import '../../di/di.dart';
 import '../../navigation/bloc/navigation_bloc.dart';
 import '../../navigation/bloc/navigation_state.dart';
 import '../../navigation/widget/bottom_navigation.dart';
-import '../../theme/expense_flow_colors.dart';
 import '../add_item/add_item_screen.dart';
 import '../categories/categories_screen.dart';
 import '../receipt_scan/receipt_scan_screen.dart';
@@ -17,7 +13,6 @@ import '../summary/summary_screen.dart';
 import '../unprocessed_receipts/unprocessed_receipts_screen.dart';
 import 'bloc/camera_preview_bloc.dart';
 import 'bloc/camera_preview_event.dart';
-import 'bloc/camera_preview_state.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -33,80 +28,23 @@ class MainScreen extends StatelessWidget {
       ],
       child: Scaffold(
         extendBody: true,
-        body: Stack(
-          children: [
-            BlocBuilder<CameraPreviewBloc, CameraPreviewState>(
-              builder: (context, state) {
-                return switch (state) {
-                  CameraPreviewInitialized() =>
-                    _CameraPreview(state.controller),
-                  CameraInitError() => Text(state.message),
-                  _ => SizedBox.shrink()
-                };
-              },
-            ),
-            BlocBuilder<NavigationBloc, NavigationState>(
-              builder: (context, state) {
-                return SafeArea(
-                    child: IndexedStack(
-                  index: state.currentIndex,
-                  children: const [
-                    ReceiptScanScreen(),
-                    AddItemScreen(),
-                    CategoriesScreen(),
-                    SummaryScreen(),
-                    UnprocessedReceiptsScreen()
-                  ],
-                ));
-              },
-            )
-          ],
+        body: BlocBuilder<NavigationBloc, NavigationState>(
+          builder: (context, state) {
+            return SafeArea(
+                child: IndexedStack(
+              index: state.currentIndex,
+              children: const [
+                ReceiptScanScreen(),
+                AddItemScreen(),
+                CategoriesScreen(),
+                SummaryScreen(),
+                UnprocessedReceiptsScreen()
+              ],
+            ));
+          },
         ),
         bottomNavigationBar: const BottomNavigation(),
       ),
     );
-  }
-}
-
-class _CameraPreview extends StatelessWidget {
-  final CameraController controller;
-
-  const _CameraPreview(this.controller);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        // TODO: temporarily disable preview in background
-        // _buildBlurredBackgroundPreview(context),
-      ],
-    );
-  }
-
-  Widget _buildBlurredBackgroundPreview(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final deviceRatio = size.width / size.height;
-    final aspectRatio = controller.value.aspectRatio;
-
-    return Container(
-        color: Theme.of(context).colorScheme.surface,
-        child: Center(
-          child: Transform.scale(
-            scale: controller.value.aspectRatio / deviceRatio,
-            child: AspectRatio(
-                aspectRatio: aspectRatio,
-                child: ImageFiltered(
-                  imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                  child: ColorFiltered(
-                    colorFilter: ColorFilter.mode(
-                      ExpenseFlowColors.darkAccentDelicate.withAlpha(250),
-                      BlendMode.modulate,
-                    ),
-                    child: CameraPreview(controller),
-                  ),
-                )),
-          ),
-        ));
   }
 }
